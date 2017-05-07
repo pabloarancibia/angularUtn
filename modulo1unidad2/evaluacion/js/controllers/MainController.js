5f11a583f184a04c9e6253d977daec677fb12160
zulema morel <zulema morel>
2017-05-07 20:25:51 -0300

    evaluacion 1 terminada

--- modulo1unidad2/evaluacion/js/controllers/MainController.js
+++ modulo1unidad2/evaluacion/js/controllers/MainController.js
@@ -18,5 +18,43 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 3.95
     }
   ];
-
+    
+    $scope.mains = [
+        {
+            name: 'Marguerita',
+            description: 'Mozarella, marinara sauce',
+            price: 6.95            
+        },
+        {
+            name: 'Pepperoni',
+            description: 'Pepperoni, marinara sauce, fresh oregano',
+            price: 7.95
+        },
+        {
+            name: 'Rustica',
+            description: 'Mozarella, marinara sauce, roasted garlic, fresh oregano',
+            price: 8.95
+        }
+    ];
+    $scope.extras = [
+        {
+            name: 'Especial',
+            description: 'Mozarella, marinara sauce',
+            price: 9.55            
+        },
+        {
+            name: 'Jamones',
+            description: 'Pepperoni, marinara sauce, fresh oregano',
+            price: 6.65
+        },
+        {
+            name: 'Planet pizza',
+            description: 'Mozarella, marinara sauce, roasted garlic, fresh oregano',
+            price: 8.65
+        }
+    ];
+    
+    $scope.filtrar = function(){
+        $scope.filtroBuscar = $scope.valFiltrar;
+    }
 }]);
