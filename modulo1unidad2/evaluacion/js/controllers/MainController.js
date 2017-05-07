503b665bb24d0aae3e59fa83aae3c56af57b1545
zulema morel <zulema morel>
2017-05-07 18:07:37 -0300

    primer commit

---
+++ modulo1unidad2/evaluacion/js/controllers/MainController.js
@@ -0,0 +1,22 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+	{
+      name: 'Rockabilly',
+      description: 'Served with presleys snacks.',
+      price: 3.95
+    }
+  ];
+
+}]);
